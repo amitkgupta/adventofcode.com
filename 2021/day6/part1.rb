f69c380570843915f11f79e672e4bd5135a9c0de
Amit Kumar Gupta <amitkgupta84@gmail.com>
2021-12-06 10:57:43 -0700

    2021/day6: fix typos in comments

--- 2021/day6/part1.rb
+++ 2021/day6/part1.rb
@@ -1,4 +1,4 @@
-#, Let, n(d)(t), be the number of fish with internal timer t on day d
+# Let n(d)(t) be the number of fish with internal timer t on day d.
 # Regarding n(d) as a 9x1 vector, n(d+1) = An(d) where
 #
 #       0 1 0 0 0 0 0 0 0
@@ -12,7 +12,7 @@
 #       1 0 0 0 0 0 0 0 0
 #
 # We want to find the sum of the elements in n(80) given n(0).
-# n(80) = (A^80)n(0)
+# n(80) = (A^80)n(0).
 
 require 'matrix'
 
